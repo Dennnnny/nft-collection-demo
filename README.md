9394fa12a26178fa884452c86a47a7cce17daa4c
denny <denny.chang@invos.com.tw>
2022-10-10 00:20:43 +0800

    remove cache nextjs

---
+++ README.md
@@ -0,0 +1,5 @@
+this is the one make nft collection stuff
+
+not really know what's going on
+
+but, fake it until make it
---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x9fe2e5c30B2fD5E7Aa0D1499A4bB04B9b8fd53b9";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
